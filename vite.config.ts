18d687dff498b1bfa436c26ba33b80934f0b0f55
vinaybetabulls <bodigevinay92@gmail.com>
2025-11-01 18:09:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['all'], // or ['.csb.app'] if you prefer specific
+  },
 })
